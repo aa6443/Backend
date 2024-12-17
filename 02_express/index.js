5fab2854ac2f3ce6b81847baa5e0bb3b896127df
Aniket Kumar <aa6443@srmist.edu.in>
2024-12-17 15:49:20 +0530

    Advance logging with Morgan and winston

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,14 +1,44 @@
 import express from "express"
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
-
 const port = 3000
+
+const morganFormat = ":method :url :status :response-time ms";
+
+
+app.use ( morgan
+    (
+        morganFormat, 
+        {
+            stream: {
+                write: (message) => {
+                    const logObject = {
+                        method: message.split(' ')[0],
+                        url: message.split(' ')[1],
+                        status: message.split(' ')[2],
+                        responseTime: message.split(' ')[3]
+                    };
+                    logger.info(JSON.stringify(logObject));
+                },
+            },
+        })
+    
+);
+
+
+
+
 
 app.use(express.json())
 let teaData  = []
 let nextId = 1
 // Add a new Tea
 app.post("/teas",(req,res)=>{
+    //this is professional method 
+    // logger.info("A post request was made to /teas");
+    // logger.warn("A post request was made to /teas");
 
     const {name,price} = req.body
     const newTea = {id: nextId++, name, price }
